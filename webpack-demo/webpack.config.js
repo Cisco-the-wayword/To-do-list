8fdcda9ef1476befd72834e3d5f2bf83ebc562b7
Cisco <ciscophrenzy@gmail.com>
2023-04-05 11:34:57 -0600

    Fix errors

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,18 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+  mode: 'development',
   entry: './src/index.js',
   devServer: {
     static: './dist',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -24,10 +16,13 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
   },
-  optimization: {
-    runtimeChunk: 'single',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
