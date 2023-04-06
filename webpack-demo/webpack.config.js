3d8f13dc7978861ae8154ed8af431b711b93b097
Cisco <ciscophrenzy@gmail.com>
2023-04-06 02:30:17 -0600

    Modify webpack

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,11 +6,6 @@ module.exports = {
   devServer: {
     static: './dist',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
