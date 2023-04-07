a79070f37b1b54ab9ea154b298c585cc3964975e
Cisco <ciscophrenzy@gmail.com>
2023-04-07 04:57:49 -0600

    Add required changes

--- webpack-demo/src/tasks.js
+++ webpack-demo/src/tasks.js
@@ -7,21 +7,24 @@ export const addTask = (description, tasks) => {
   const task = {
     description,
     completed: false,
-    index: tasks.length + 1,
+    index: tasks.length,
   };
   tasks.push(task);
   saveTasks(tasks);
   return task;
 };
 
+
 // Function for deleting or removing tasks
 export const deleteTask = (index, tasks) => {
   tasks.splice(index, 1);
-  tasks.forEach((task, i) => {
-    task.index = i;
-  });
+  for (let i = index; i < tasks.length; i++) {
+    tasks[i].index = i + 1;
+  }
   saveTasks(tasks);
 };
+
+
 
 // Function for editing task description.
 export const editTask = (index, description, tasks) => {
